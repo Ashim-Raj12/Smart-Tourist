3e99fdccca29999558afde0d261a638db4cd692e
Ashim Raj <programwithashim@gmail.com>
2025-09-13 02:56:45 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://smart-tourist-backend.onrender.com',
+  baseURL: 'https://smart-tourist-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
